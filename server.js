75f4ad094773ae96feb6ae539e078d369f8f2be8
piyushbinjhade <piyushbinjhade2019@gmail.com>
2026-04-15 02:10:53 +0530

    feat: add root endpoint to confirm backend is working

--- server.js
+++ server.js
@@ -325,6 +325,10 @@ app.post("/ingest/github", async (req, res) => {
 });
 
 // (Render compatible)
+
+app.get("/", (req, res) => {
+  res.send("BACKEND WORKING");
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
